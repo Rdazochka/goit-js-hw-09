3de2f4fa1de2ae0d25225543adb47a0d2c9c5a57
Rdazochka <rdazochka@gmail.com>
2026-05-10 05:59:21 +0000

    Deploying to gh-pages from @ Rdazochka/goit-js-hw-09@52584766697f0e33f9379c22e423a0d4bd6d3138 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,12 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const n=`<form class="feedback-form" autocomplete="off">
-  <label>
-    Email
-    <input type="email" name="email" autofocus />
-  </label>
-  <label>
-    Message
-    <textarea name="message" rows="1"></textarea>
-  </label>
-  <button type="submit">Submit</button>
-</form>`,m=document.querySelector(".form-container");m.insertAdjacentHTML("afterbegin",n);const r="feedback-form-state",a={email:"",message:""},t=document.querySelector(".feedback-form");t.querySelector("textarea");t.addEventListener("input",e=>{const{name:o,value:l}=e.target;a[o]=l;const s=JSON.stringify(a);localStorage.setItem(r,s)});document.addEventListener("DOMContentLoaded",()=>{const e=c(r);e&&(t.elements.email.value=e.email,t.elements.message.value=e.message)});t.addEventListener("submit",e=>{if(e.preventDefault(),!a.email||!a.message){alert("Fill please all fields");return}console.log(a),localStorage.removeItem(r),t.reset()});function c(e){const o=localStorage.getItem(e);return JSON.parse(o)}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const r="feedback-form-state",a={email:"",message:""},t=document.querySelector(".feedback-form");t.querySelector("textarea");t.addEventListener("input",e=>{const{name:o,value:s}=e.target;a[o]=s;const n=JSON.stringify(a);localStorage.setItem(r,n)});document.addEventListener("DOMContentLoaded",()=>{const e=l(r);e&&(t.elements.email.value=e.email,t.elements.message.value=e.message)});t.addEventListener("submit",e=>{if(e.preventDefault(),!a.email||!a.message){alert("Fill please all fields");return}console.log(a),localStorage.removeItem(r),t.reset()});function l(e){const o=localStorage.getItem(e);return JSON.parse(o)}
 //# sourceMappingURL=2-form.js.map
